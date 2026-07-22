eedca6324b63b15bd0cd82ff2cd1659a5b35690a
Sameer <jangidsameer77@gmail.com>
2026-07-22 19:32:03 +0530

    Server Setup

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,14 @@ import app from "./app.js";
 
 const PORT = 8000;
 
-try {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT} `);
-  });
-} catch (err) {
-  console.log("Error in running the server : ", err);
-}
+const startServer = () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT} `);
+    });
+  } catch (err) {
+    console.log("Error in running the server : ", err);
+  }
+};
+
+startServer();
