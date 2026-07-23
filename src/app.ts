e3aac173a18374d1ef05abfc9805ecf8428de644
Sameer <jangidsameer77@gmail.com>
2026-07-23 23:47:13 +0530

    feat: update dependencies and improve app configuration with CORS and global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
+import cookieParser from "cookie-parser";
 import express from "express";
+import cors from "cors";
+import { env } from "./config/env.js";
+import { globalErrorHandler } from "./middleware/error.middleware.js";
+
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: env.FRONTEND_URL,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
 
+app.use(globalErrorHandler)
 export default app;
--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const env = {
   PORT: process.env.PORT || 8000,
   DATABASE_URL: process.env.DATABASE_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL
 };
